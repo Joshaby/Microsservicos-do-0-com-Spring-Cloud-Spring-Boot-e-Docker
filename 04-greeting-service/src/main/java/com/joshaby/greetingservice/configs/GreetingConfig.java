e9c28692c82e85bb686356155330f31d931e15e6
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-01-14 17:14:21 -0300

    Adiciona anotação para receber atualização das propriedades do configserver

--- 04-greeting-service/src/main/java/com/joshaby/greetingservice/configs/GreetingConfig.java
+++ 04-greeting-service/src/main/java/com/joshaby/greetingservice/configs/GreetingConfig.java
@@ -3,10 +3,12 @@ package com.joshaby.greetingservice.configs;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "greeting-service")
+@RefreshScope
 @Getter
 @Setter
 public class GreetingConfig {
