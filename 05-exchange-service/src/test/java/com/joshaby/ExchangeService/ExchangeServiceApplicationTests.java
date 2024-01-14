df5cd538e48990f54cedb9ee22ebdf7d6cab7c8e
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-01-14 17:34:10 -0300

    Adiciona projeto exchange-service

---
+++ 05-exchange-service/src/test/java/com/joshaby/ExchangeService/ExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshaby.ExchangeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
