ab361c43bb2e5e1166adf3e98bba6c19c8252820
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-01-15 15:30:53 -0300

    Adiciona método de obtenção dinâmica da porta de execução

--- 05-exchange-service/src/main/java/com/joshaby/ExchangeService/controllers/ExchangeController.java
+++ 05-exchange-service/src/main/java/com/joshaby/ExchangeService/controllers/ExchangeController.java
@@ -1,6 +1,8 @@
 package com.joshaby.ExchangeService.controllers;
 
 import com.joshaby.ExchangeService.models.Exchange;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,15 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("exchange-service")
+@RequiredArgsConstructor
 public class ExchangeController {
+
+    private final Environment environment;
 
     @GetMapping("/{amount}/{from}/{to}")
     public Exchange find(@PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+
+        String port = environment.getProperty("local.server.port");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
     }
 }
