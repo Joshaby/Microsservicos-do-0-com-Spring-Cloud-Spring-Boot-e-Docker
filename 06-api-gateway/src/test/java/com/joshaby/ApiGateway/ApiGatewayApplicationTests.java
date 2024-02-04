7aeed084207e086200cf31a80a18db1ef7ec8cee
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-02-04 16:15:41 -0300

    Adiciona projeto api-gateway

---
+++ 06-api-gateway/src/test/java/com/joshaby/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshaby.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
