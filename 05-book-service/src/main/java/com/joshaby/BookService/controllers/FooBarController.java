1e39645dec10965438b8231850f8a12ebd9f2a6d
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-02-10 16:58:36 -0300

    Adiciona fallback method e configurações no yml

--- 05-book-service/src/main/java/com/joshaby/BookService/controllers/FooBarController.java
+++ 05-book-service/src/main/java/com/joshaby/BookService/controllers/FooBarController.java
@@ -1,7 +1,6 @@
 package com.joshaby.BookService.controllers;
 
 import io.github.resilience4j.retry.annotation.Retry;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +13,14 @@ import org.springframework.web.client.RestTemplate;
 public class FooBarController {
 
     @GetMapping("/foo-bar")
-    @Retry(name = "default")
+    @Retry(name = "default", fallbackMethod = "fallbackMethod")
     public String fooBar() {
         log.info("Resquest to foo-bar is received!");
         var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
         return response.getBody();
+    }
+
+    public String fallbackMethod(Exception e) {
+        return "fallbackMethod foo-bar";
     }
 }
