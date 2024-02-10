4da52bfb943d7c61021b7a75eddd98f3ac31d2b4
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-02-10 16:47:04 -0300

    Atualiza dependências do Resilience4j e adiciona uso do Retry no FooBarController

--- 05-book-service/src/main/java/com/joshaby/BookService/controllers/FooBarController.java
+++ 05-book-service/src/main/java/com/joshaby/BookService/controllers/FooBarController.java
@@ -1,15 +1,23 @@
 package com.joshaby.BookService.controllers;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/book-service")
+@Slf4j
 public class FooBarController {
 
     @GetMapping("/foo-bar")
+    @Retry(name = "default")
     public String fooBar() {
-        return "Foo-Bar!!";
+        log.info("Resquest to foo-bar is received!");
+        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return response.getBody();
     }
 }
