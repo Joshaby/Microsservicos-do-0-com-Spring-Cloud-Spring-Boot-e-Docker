f60c7566da061314caff22e4d606576b3f2c7a36
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2024-02-12 18:36:33 -0300

    Adiciona depedência Open AI API e UI no exchange-service

--- 05-exchange-service/src/main/java/com/joshaby/ExchangeService/controllers/ExchangeController.java
+++ 05-exchange-service/src/main/java/com/joshaby/ExchangeService/controllers/ExchangeController.java
@@ -2,6 +2,8 @@ package com.joshaby.ExchangeService.controllers;
 
 import com.joshaby.ExchangeService.models.Exchange;
 import com.joshaby.ExchangeService.repositories.ExchangeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Exchange Endpoint")
 @RestController
 @RequestMapping("exchange-service")
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class ExchangeController {
 
     private final ExchangeRepository repository;
 
+    @Operation(summary = "Get exchange from currency")
     @GetMapping("/{amount}/{from}/{to}")
     public Exchange find(@PathVariable BigDecimal amount, @PathVariable String from, @PathVariable String to) {
 
